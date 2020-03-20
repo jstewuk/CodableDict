9780451da92ed34606443172e8954b81b00e8c03
Jim Stewart <jim@stewartstuff.com>
2020-03-20 08:15:46 -0500

    Initial Commit

---
+++ CodableDict/CodableDict.h
@@ -0,0 +1,19 @@
+//
+//  CodableDict.h
+//  CodableDict
+//
+//  Created by James Stewart on 3/20/20.
+//  Copyright © 2020 James Stewart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodableDict.
+FOUNDATION_EXPORT double CodableDictVersionNumber;
+
+//! Project version string for CodableDict.
+FOUNDATION_EXPORT const unsigned char CodableDictVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableDict/PublicHeader.h>
+
+
